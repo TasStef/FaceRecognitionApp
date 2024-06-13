72ca616a47ebe6fa936902016bdbe7acc5a94233
Tasos Stefanidis <tasos.stefanidis@centtrip.com>
2024-06-13 22:20:35 +0100

    Implementing database comunication and bug fixes

--- apis/server.js
+++ apis/server.js
@@ -44,66 +44,75 @@ app.listen(3000, () => {
   console.log("App is running on 3000");
 });
 
-// Root
+//! Root
 app.get("/", (req, res) => {
   res.send("It's working");
 });
 
-// Sign In
+//! Sign In
 app.post("/signin", (req, res) => {
-  // Load hash from your password DB.
-  bcrypt.compare(
-    "apples",
-    "$2a$10$L3YDwjALwUyk6Su5NRllPumsjl5xbO4iIF9B.CI5.Wl7Tn6rERTuO",
-    function (err, res) {
-      console.log("First response: ", res);
-    }
-  );
-  bcrypt.compare(
-    "veggies",
-    "$2a$10$L3YDwjALwUyk6Su5NRllPumsjl5xbO4iIF9B.CI5.Wl7Tn6rERTuO",
-    function (err, res) {
-      console.log("First response: ", res);
-    }
-  );
-
-  const email = req.body.email;
-
-  if (
-    email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("Wrong email or password");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json(`Error within signin ${err}`));
+      } else {
+        return res.status(400).json("Wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("Wrong credentials"));
 });
 
-// Register
+//! Register
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
 
-  let passwordHashed;
-  bcrypt.hash(password, null, null, function (err, hash) {
-    passwordHashed = hash;
-  });
+  let hash = bcrypt.hashSync(password);
 
-  db("users")
-    .returning("*")
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => res.status(400).json(`Error in register: ${err}`)); //in prod this should be more cryptic "unable to register"
+  // transaction allows for rollback in the Database if somethign goes wrong
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0].email,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user);
+          })
+          .catch((err) => res.status(400).json(`Error in register: ${err}`));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json(`Error in register: ${err}`)); //in prod this should be more cryptic "unable to register";
 });
 
-// Get Users
+//! Get Users
 app.get("/users", (req, res) => {
-  res.json(database);
+  db.select("*")
+    .from("users")
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(404).json(`Error fetching users`));
 });
 
 // Profile
